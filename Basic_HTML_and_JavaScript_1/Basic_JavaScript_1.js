3d85584b48c6532151385891e3bc984b4623153b
suniyang <99436860+suniyang@users.noreply.github.com>
2024-03-01 13:24:21 +0000

    HTML assignments

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,4 @@
+
+alert("Hello, World! I am an alert box!");
+
+<script>window.onload = function() { document.getElementById("myParagraph").innerHTML = "This is a new paragraph content."; };</script>
